ba8180fc7eaf0810913953dcef030b9d1c021873
haryoiro <55312590+haryoiro@users.noreply.github.com>
2019-11-28 18:26:46 +0900

    2019/11/28 -002

--- 007/app.js
+++ 007/app.js
@@ -1,1 +1,14 @@
-const expres = require('express')
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res) {
+  return  res.send('Hello Express')
+})
+
+app.get('/instructor/:firstName', (req, res) => {
+  return res.send(`This instructor\'s firstName is ${req.params.firstName}`)
+})
+
+app.listen(3000, function() {
+  console.log("The server has started on port 3000. Head to localhost:3000 in the browser and see what's there!")
+})
