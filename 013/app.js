6a887580b19a29b1e34377409ba6e953873bf435
haryoiro <55312590+haryoiro@users.noreply.github.com>
2019-12-05 23:48:49 +0900

    2019/12/05 お絵かきチャットの素を実装できた。

--- 013/app.js
+++ 013/app.js
@@ -9,7 +9,16 @@ app.use(express.static('public'))
 const io = socket(server)
 
 const connection = socket => {
-  console.log(socket)
+  console.log(`new connection: ${socket.id}`)
+  socket.on('mousePoint', data => {
+    socket.broadcast.emit('mousePoint', data)
+  })
+  socket.on('mouseDown', data => {
+    socket.broadcast.emit('mouseDown', data)
+  })
+  socket.on('mouseUp', data => {
+    socket.broadcast.emit('mouseUp', data)
+  })
 }
 
 io.sockets.on('connection', connection)
--- 013/public/index.js
+++ 013/public/index.js
@@ -2,12 +2,34 @@ window.addEventListener('load', () => {
   const canvas = document.querySelector('#canvas')
   const ctx = canvas.getContext('2d')
   const socket = io.connect('http://localhost:3000')
+  
+  socket.on('mousePoint', data => {
+  if(dragging) {
+    ctx.lineTo(data.x, data.y)
+    ctx.stroke();
+    ctx.beginPath()
+    ctx.arc(data.x, data.y, radius, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.beginPath()
+    ctx.moveTo(data.x, data.y)
+  }
+  })
+
+  socket.on('mouseUp', data => {
+    dragging = false
+    ctx.beginPath()
+  })
+
+  socket.on('mouseDown', data => {
+    dragging = true
+  })
+
 
   let radius = 10,
     dragging = false
-    w = canvas.width = 1920;
-    h = canvas.height = 1080;
-    ctx.lineWidth = radius * 2;
+    w = canvas.width = 1920 / 4
+    h = canvas.height = 1080 / 4
+    ctx.lineWidth = radius * 2
 
   const drawPoint = (e) => {
     if(dragging) {
@@ -21,19 +43,27 @@ window.addEventListener('load', () => {
           ctx.fill()
           ctx.beginPath()
           ctx.moveTo(x, y)
+      let data = {
+        x: x,
+        y: y
+      }
+      socket.emit('mousePoint', data)
     }
   }
 
   const disengage = (e) => {
     dragging = false
     ctx.beginPath()
+    socket.emit('mouseUp')
   }
 
   const engage = () => {
     dragging = true
+    socket.emit('mouseDown')
   }
 
   canvas.addEventListener('mousedown', engage)
   canvas.addEventListener('mousemove', drawPoint)
   canvas.addEventListener('mouseup', disengage)
 })
+
