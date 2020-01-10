d47b6fba29beba52255d28a194cfcb14fcd1f34c
Haryoiro <mizusecocolt@gmail.com>
2020-01-10 22:26:01 +0900

    2020/01/10 React click counter

--- 191229-/002/src/app.js
+++ 191229-/002/src/app.js
@@ -3,16 +3,28 @@ import ReactDOM from 'react-dom'
 import styles from './css/app.css'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+    this.onClickButton = this.onClickButton.bind(this)
+  }
+  onClickButton() {
+    let { count } = this.state
+    this.setState({count: count + 1})
+  }
   render() {
     return(
       <div id = "main">
-        <h1 className = "title"> {"HELLO REACT"} </h1>
+        <h1>{`クリックした回数 ${this.state.count}`}</h1>
+        <button onClick={this.onClickButton}>Button</button>
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <App name={"HARYO"}/>,
   document.getElementById('app')
 )
