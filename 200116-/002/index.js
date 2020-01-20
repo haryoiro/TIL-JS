77125da17e7c63c5cd32fc747c55cb2c5525eee7
haryoiro <mizusecocolte@gmail.com>
2020-01-20 16:46:05 +0900

    2020/01/20

---
+++ 200116-/002/index.js
@@ -0,0 +1,140 @@
+const	CHRWIDTH	= 8				// キャラはば
+const	CHRHEIGHT	= 9				// キャラ高さ
+const	FONT 		= "20px Arial"	// フォント
+const	HEIGHT 		= 120			// 仮想画面縦
+const	WIDTH 		= 120			// 仮想画面横
+const	MAP_WIDTH	= 32			// マップ幅
+const	MAP_HEIGHT	= 32			// マップ高さ
+const	SMOOTH		= 0				// 補完処理
+const	TILESIZE	= 8				// タイルサイズ	
+const	TILECOLUMN	= 4
+const	TILEROW		= 4
+
+let 	gFrame = 0				// 内部カウンタ
+let		gImgMap					// マップ画像
+let		gImgPl				// キャラ画像
+let		gPlayerX = 10
+let		gPlayerY = 20
+let		gScreen					// 仮想画面
+let 	gWidth					// 実画面横
+let 	gHeight					// 実画面縦
+
+const	gMap = [
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 3, 6, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 3, 3, 6, 6, 7, 7, 7, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 6, 3, 0, 0, 0, 3, 3, 0, 6, 6, 6, 0, 0, 0,
+	0, 0, 3, 3, 6, 6, 6, 7, 7, 2, 2, 2, 7, 7, 2, 2, 2, 7, 7, 6, 3, 3, 3, 6, 6, 3, 6,13, 6, 0, 0, 0,
+	0, 3, 3,10,11, 3, 3, 6, 7, 7, 2, 2, 2, 2, 2, 2, 1, 1, 7, 6, 6, 6, 6, 6, 3, 0, 6, 6, 6, 0, 0, 0,
+	0, 0, 3, 3, 3, 0, 3, 3, 3, 7, 7, 2, 2, 2, 2, 7, 7, 1, 1, 6, 6, 6, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 7, 7, 7, 7, 2, 7, 6, 3, 1, 3, 6, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 7, 2, 7, 6, 3, 1, 3, 3, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 0, 3, 3, 3, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 3,12, 3, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 7, 7, 6, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 6, 6, 6, 6, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 6, 6, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 1, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 3, 3, 3, 6, 6, 6, 3, 3, 3, 1, 1, 1, 1, 1, 3, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 8, 9, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3, 1, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 3, 3, 3, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0,
+	7,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 0, 0, 0, 0, 0,
+	7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7,
+]
+
+
+function wSize(){
+	const ca = document.getElementById('canvas')
+	ca.width = innerWidth
+	ca.height = innerHeight
+
+	const g = ca.getContext('2d')
+	g.imageSmoothingEnabled = g.msImageSmoothingEnabled = SMOOTH	// 補完処理
+
+	// 実画面サイズを計測
+	// ドットのアスペクト比を維持したままでの最大サイズを計測する
+	gWidth = ca.width 
+	gHeight = ca.height
+	if( gWidth / WIDTH < gHeight / HEIGHT) {
+		gHeight = gWidth * HEIGHT / WIDTH
+	} else {
+		gWidth = gHeight * WIDTH / HEIGHT
+	}
+}
+
+function loadImage(){
+	gImgMap = new Image(),	gImgMap.src = 'img/map.png'
+	gImgPl	= new Image(),	gImgPl.src	= 'img/player.png'
+}
+
+// ゲームメイングラフィック
+function gGraphic(){
+
+	const g =  gScreen.getContext('2d')
+
+	// Mapの展開
+	for(let y = 0; y < 20; y++){
+		for(let x = 0; x < 20; x++){
+			let	px = gPlayerX + x
+			let py = gPlayerY + y
+			drawTile(g, x * TILESIZE, y * TILESIZE, gMap[ py * MAP_WIDTH + px])
+		}
+	}
+
+	g.drawImage(gImgPl, 
+				CHRWIDTH, 0, CHRWIDTH, CHRHEIGHT, 
+				WIDTH / 2, HEIGHT / 2, CHRWIDTH, CHRHEIGHT)
+
+	gFrame++
+	if(gFrame >= 100)gFrame = 0
+
+	// g.font = FONT
+	// g.fillStyle = 'black'
+	// g.fillText(gFrame, 5, 30)
+}
+
+function drawTile(g, x, y, index) {
+	const ix = (index % TILECOLUMN) * TILESIZE 
+	const iy = Math.floor(index / TILECOLUMN) * TILESIZE
+	g.drawImage(gImgMap, ix, iy, TILESIZE, TILESIZE, x, y, TILESIZE, TILESIZE)
+		
+}
+
+function wTimer(){
+
+	gGraphic()
+
+	const ca = document.getElementById('canvas')
+	const g =  ca.getContext('2d')
+
+	// 仮想画面のイメージを実画面に転送
+	g.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, gWidth, gHeight)
+
+}
+
+(() => {
+
+	gScreen = document.createElement('canvas')	// 仮想画面の設定
+	gScreen.width = WIDTH						// 仮想画面の幅を設定	
+	gScreen.height = HEIGHT
+
+	loadImage()
+
+	wSize()	// 画面サイズ初期化
+	window.addEventListener('resize', wSize)
+
+	// フレームレートを設定
+	setInterval(() => {wTimer()}, 33)
+})()
